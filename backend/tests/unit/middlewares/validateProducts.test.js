c6601d63912a79d1265533215fb50d2e48e06004
Clinton Brito <clintoncsb@gmail.com>
2023-10-30 13:42:32 -0300

    ✅ test: validateProducts middleware ok

--- backend/tests/unit/middlewares/validateProducts.test.js
+++ backend/tests/unit/middlewares/validateProducts.test.js
@@ -11,21 +11,53 @@ const { productInvalidFromModel } = require('../mocks/productsModel.mock');
 const { productUpdatedOkFromModel } = require('../mocks/productsService.mock');
 
 describe('Test - Products Middleware:', function () {
-  // it('should return an object with status 400 after trying to create a product with invalid `name`', async function () {
-  //   sinon.stub(productsModel, 'productIdExistsInDB').resolves(productWithInvalidNameFromModel);
+  it('should return an object with status 400 after trying to create a product with invalid `name`', async function () {
+    sinon.stub(productsModel, 'productIdExistsInDB').resolves(); // O que eu deveria colocar aqui?
 
-  //   const name = 'Asdf';
-  //   const req = { body: { name } };
-  //   const res = { status: sinon.stub().returnsThis(), json: sinon.stub() };
-  //   const next = sinon.stub();
+    const name = null;
+    const req = { body: { name } };
+    const res = { status: sinon.stub().returnsThis(), json: sinon.stub() };
+    const next = sinon.stub();
   
-  //   await validateProducts.validateProducts(req, res, next);
-  //   // console.log(productUpdated);
-  //   const expectedResult = { status: 400, data: { message: 'Name is required' } };
+    await validateProducts.validateProducts(req, res, next);
+    // console.log(productUpdated);
+    const expectedResult = { status: 400, data: { message: '"name" is required' } };
 
-  //   expect(res.status).to.have.been.calledWith(expectedResult.status);
-  //   expect(res.json).to.have.been.calledWith(expectedResult.data);
-  // });
+    expect(res.status).to.have.been.calledWith(expectedResult.status);
+    expect(res.json).to.have.been.calledWith(expectedResult.data);
+  });
+
+  it('should return an object with status 422 after trying to create a product with less than 5 characters for `name`', async function () {
+    sinon.stub(productsModel, 'productIdExistsInDB').resolves(); // O que eu deveria colocar aqui?
+
+    const name = 'Bill';
+    const req = { body: { name } };
+    const res = { status: sinon.stub().returnsThis(), json: sinon.stub() };
+    const next = sinon.stub();
+  
+    await validateProducts.validateProducts(req, res, next);
+    // console.log(productUpdated);
+    const expectedResult = { status: 422, data: { message: '"name" length must be at least 5 characters long' } };
+
+    expect(res.status).to.have.been.calledWith(expectedResult.status);
+    expect(res.json).to.have.been.calledWith(expectedResult.data);
+  });
+
+  it('`next()`should be called after trying to create a product with correct inputs', async function () {
+    sinon.stub(productsModel, 'productIdExistsInDB').resolves(); // O que eu deveria colocar aqui?
+
+    const name = 'Laço da Verdade';
+
+    const req = { body: { name } };
+    const res = { status: sinon.stub().returnsThis(), json: sinon.stub() };
+    const next = sinon.stub();
+  
+    await validateProducts.validateProducts(req, res, next);
+    // console.log(productUpdated);
+    // const expectedResult = { status: 200, data: productUpdatedOkFromModel };
+
+    expect(next).to.have.been.calledWith();
+  });
 
   it('should return an object with status 404 after trying to update an invalid product', async function () {
     sinon.stub(productsModel, 'productIdExistsInDB').resolves(productInvalidFromModel);
